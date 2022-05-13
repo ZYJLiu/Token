5a544bb2fc6dd3516a046f5e988fbab677f09c0b
john <johnzhengyuliu@gmail.com>
2022-05-12 21:11:19 -0500

    working

--- programs/token/src/lib.rs
+++ programs/token/src/lib.rs
@@ -1,15 +1,226 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+ // REPLACE ADDRESS of diam mint by running solana address -k .keys/usdc_mint.json
+pub const USDC_MINT_ADDRESS: &str = "8fFnX9WSPjJEADtG5jQvQQptzfFmmjd6hrW7HjuUT8ur";
+  
+
 #[program]
-pub mod token {
+pub mod tokens {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_token_account(ctx: Context<CreateTokenAccount>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn create_mint(ctx: Context<CreateMint>, name: String) -> Result<()> {
+        let (pda, bump) =
+            Pubkey::find_program_address(&[ctx.accounts.merchant.key().as_ref()], ctx.program_id);
+
+        let merchant = &mut ctx.accounts.merchant;
+        merchant.name = name;
+        merchant.mint = pda;
+        merchant.bump = bump;
+        Ok(())
+    }
+
+     pub fn mint_to(ctx: Context<MintTo>, amount: u64) -> Result<()> {
+        let merchant = ctx.accounts.merchant.key();
+
+
+        let seeds = &[merchant.as_ref(), &[ctx.accounts.merchant.bump]];
+        let signer = [&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::MintTo {
+                mint: ctx.accounts.mint_pda.to_account_info(),
+                to: ctx.accounts.user_token.to_account_info(),
+                authority: ctx.accounts.mint_pda.to_account_info(),
+            },
+            &signer,
+        );
+        token::mint_to(cpi_ctx, amount)?;
+
+        // transfer USDC from the User to Program
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.user_usdc_token.to_account_info(),
+                authority: ctx.accounts.user.to_account_info(),
+                to: ctx.accounts.program_usdc_token.to_account_info(),
+            },
+        );
+        token::transfer(cpi_ctx, amount)?;
+
+
+        Ok(())
+    }
+
+       pub fn burn(ctx: Context<Burn>, amount: u64,) -> Result<()> {
+
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Burn {
+                mint: ctx.accounts.mint_pda.to_account_info(),
+                from: ctx.accounts.user_token.to_account_info(),
+                authority: ctx.accounts.user.to_account_info(),
+            },
+        );
+        token::burn(cpi_ctx, amount)?;
+
+        // transfer USDC merchant.
+        let (usdc_pda, usdc_bump) = Pubkey::find_program_address(&[ctx.accounts.usdc_mint.key().as_ref()], ctx.program_id);
+        let usdc_mint_address = ctx.accounts.usdc_mint.key();
+        let seeds = &[usdc_mint_address.as_ref(), &[usdc_bump]];
+        let signer = [&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.program_usdc_token.to_account_info(),
+                authority: ctx.accounts.program_usdc_token.to_account_info(),
+                to: ctx.accounts.merchant_usdc_token.to_account_info(),
+            },
+            &signer,
+        );
+
+        let usdc_amount = amount; // TODO: Change the formula
+        token::transfer(cpi_ctx, usdc_amount)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateTokenAccount<'info> {
+    // Create Program USDC Token Account using PDA
+    #[account(
+        init,
+        payer = payer,
+        seeds = [mint.key().as_ref() ],
+        bump,
+        token::mint = mint,
+        token::authority = token_account,
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+
+    // "USDC" Mint
+    // TODO: add check
+    pub mint: Account<'info, Mint>,
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String)]
+pub struct CreateMint<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 100 // TODO: calculate space
+    )]
+    pub merchant: Account<'info, Merchant>,
+
+    #[account(
+        init,
+        seeds = [merchant.key().as_ref()],
+        bump,
+        payer = user,
+        mint::decimals = 2,
+        mint::authority = mint_pda, 
+        
+    )]
+    pub mint_pda: Account<'info, Mint>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: Program<'info, Token>,
+}
+
+
+#[derive(Accounts)]
+pub struct MintTo<'info> {
+    #[account()]
+    pub merchant: Account<'info, Merchant>,
+    #[account(mut)]
+    pub mint_pda: Account<'info, Mint>,
+
+    // Mint Tokens here
+    #[account(mut)]
+    pub user_token: Account<'info, TokenAccount>,
+
+    // USDC from here
+    #[account(mut)]
+    pub user_usdc_token: Account<'info, TokenAccount>,
+    
+    pub user: Signer<'info>,
+
+    // USDC to here
+    #[account(
+        mut,
+        // TODO: add check
+        // seeds = [ usdc_mint.key().as_ref() ],
+        // bump = program_usdc_bag_bump,
+    )]
+    pub program_usdc_token: Box<Account<'info, TokenAccount>>,
+
+    // SPL Token Program
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+// #[instruction(usdc_bump: u8, jun_bump:u8)]
+pub struct Burn<'info> {
+    //NEED TO CHECK
+    // `token::Burn.mint`
+    #[account(mut)]
+    pub mint_pda: Box<Account<'info, Mint>>,
+
+    // `token::Burn.to`
+    #[account(mut)]
+    pub user_token: Box<Account<'info, TokenAccount>>,
+
+    // The authority allowed to mutate the above ⬆
+    pub user: Signer<'info>,
+
+    // see `token::Transfer.from`
+    #[account(
+        mut,
+        seeds = [USDC_MINT_ADDRESS.parse::<Pubkey>().unwrap().as_ref()],
+        bump,
+    )]
+    pub program_usdc_token: Box<Account<'info, TokenAccount>>,
+
+    //NEED TO CHECK
+    //Require for the PDA above 
+    #[account(
+        address = USDC_MINT_ADDRESS.parse::<Pubkey>().unwrap(),
+    )]
+    pub usdc_mint: Box<Account<'info, Mint>>,
+
+    // see `token::Transfer.to`
+    #[account(mut)]
+    pub merchant_usdc_token: Box<Account<'info, TokenAccount>>,
+
+    // SPL Token Program
+    pub token_program: Program<'info, Token>,
+
+}
+
+
+#[account]
+pub struct Merchant {
+    pub name: String,
+    pub mint: Pubkey,
+    pub bump: u8,
+    pub discount: u8,
+    pub cash_back: u8
+}
--- tests/token.ts
+++ tests/token.ts
@@ -1,16 +1,338 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { Token } from "../target/types/token";
+import { Tokens } from "../target/types/token";
+import {
+  PublicKey,
+  Keypair,
+  SystemProgram,
+  SYSVAR_RENT_PUBKEY,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+
+import {
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  getMint,
+  getOrCreateAssociatedTokenAccount,
+  createAssociatedTokenAccount,
+  getAccount,
+  createMint,
+  mintTo,
+  Account,
+  transfer,
+} from "@solana/spl-token";
+
+import fs from "fs";
+
+const initialAmount = 100;
+
+let tokenAuthority: Keypair;
+
+let usdcMint: PublicKey;
+let usdcTokenAccount: Account;
+
+let usdcPDA: PublicKey;
+let usdcBump: Number;
+
+let diam: Keypair;
+let diamMint: PublicKey;
+let diamBump: Number;
 
 describe("token", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.Token as Program<Token>;
+  const connection = anchor.getProvider().connection;
+  const program = anchor.workspace.Tokens as Program<Tokens>;
+  const userWallet = anchor.workspace.Tokens.provider.wallet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const randomPayer = async (lamports = LAMPORTS_PER_SOL) => {
+    const wallet = Keypair.generate();
+    const signature = await connection.requestAirdrop(
+      wallet.publicKey,
+      lamports
+    );
+    await connection.confirmTransaction(signature);
+    return wallet;
+  };
+
+  before(async () => {
+    tokenAuthority = Keypair.generate();
+    const signature = await connection.requestAirdrop(
+      tokenAuthority.publicKey,
+      LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(signature);
+
+    usdcMint = await createMint(
+      connection, //connection to Solana
+      await randomPayer(), //user randomPayer helper to create accounts for test
+      tokenAuthority.publicKey, // mint authority
+      null, // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
+      2, // decimals
+      usdcMintKeypair
+    );
+
+    usdcTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection, // connection to Solana
+      await randomPayer(), // randomPayer for testing
+      usdcMint, // Token Mint
+      tokenAuthority.publicKey // user with Authority over this Token Account
+    );
+
+    await mintTo(
+      connection, // connection to Solana
+      await randomPayer(), // randomPayer as payer for test
+      usdcMint, // USDC Token Mint
+      usdcTokenAccount.address, // User USDC Token Account (destination)
+      tokenAuthority, // Mint Authority (required as signer)
+      initialAmount
+    );
+
+    // check tokens minted to Token Account
+    const usdcAccount = await getAccount(connection, usdcTokenAccount.address);
+    console.log("USDC Mint:", usdcMint.toString());
+    console.log("setup USDC Token Account:", Number(usdcAccount.amount));
+  });
+
+  it("Create usdcPDA Token Account", async () => {
+    [usdcPDA, usdcBump] = await PublicKey.findProgramAddress(
+      [usdcMintAddress.toBuffer()],
+      program.programId
+    );
+
+    try {
+      await program.rpc.createTokenAccount({
+        accounts: {
+          tokenAccount: usdcPDA,
+          mint: usdcMint,
+          payer: userWallet.publicKey,
+          systemProgram: SystemProgram.programId,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          rent: SYSVAR_RENT_PUBKEY,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const Account = await getAccount(connection, usdcPDA);
+    console.log("usdcPDA Token Account:", usdcPDA.toString());
+    console.log("usdcPDA Balance:", Number(Account.amount));
+  });
+
+  it("Create Diam Mint", async () => {
+    diam = diamKeypair;
+
+    [diamMint, diamBump] = await PublicKey.findProgramAddress(
+      [diamAddress.toBuffer()],
+      program.programId
+    );
+
+    try {
+      await program.rpc.createMint("diam", {
+        accounts: {
+          merchant: diam.publicKey,
+          mintPda: diamMint,
+          user: userWallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        },
+        signers: [diamKeypair],
+      });
+
+      // get Token Mint Address
+      const diamAddress = await getMint(connection, diamMint);
+      console.log("DIAM Authority:", diamAddress.mintAuthority.toString());
+      console.log("DIAM Address:", diamMint.toString());
+
+      // assert.isTrue(diamAddress.mintAuthority.equals(diamMint));
+    } catch (error) {
+      console.log(error);
+    }
+
+    let merchantAccount2 = await program.account.merchant.fetch(diam.publicKey);
+
+    console.log("DIAM Mint:", merchantAccount2.mint.toString());
+  });
+
+  it("Mint DIAM Tokens", async () => {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const TokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      await randomPayer(),
+      diamMint,
+      provider.wallet.publicKey
+    );
+
+    const usdcTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      await randomPayer(),
+      usdcMint,
+      provider.wallet.publicKey
+    );
+
+    await mintTo(
+      connection, // connection to Solana
+      await randomPayer(), // randomPayer as payer for test
+      usdcMint, // USDC Token Mint
+      usdcTokenAccount.address, // User USDC Token Account (destination)
+      tokenAuthority, // Mint Authority (required as signer)
+      initialAmount
+    );
+
+    const data = await program.account.merchant.fetch(diam.publicKey);
+    console.log(data);
+
+    try {
+      await program.rpc.mintTo(new anchor.BN(initialAmount), {
+        accounts: {
+          merchant: diam.publicKey,
+          mintPda: diamMint,
+          userToken: TokenAccount.address,
+          userUsdcToken: usdcTokenAccount.address,
+          user: provider.wallet.publicKey,
+          programUsdcToken: usdcPDA,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const balance = (
+      await connection.getTokenAccountBalance(TokenAccount.address)
+    ).value.amount;
+
+    const balance2 = (await connection.getTokenAccountBalance(usdcPDA)).value
+      .amount;
+
+    const balance3 = (
+      await connection.getTokenAccountBalance(usdcTokenAccount.address)
+    ).value.amount;
+
+    console.log("userDIAM Balance:", balance);
+    console.log("usdcPDA Balance:", balance2);
+    console.log("userUSDC Balance:", balance3);
+
+    // assert.equal(balance, initialAmount);
+  });
+
+  it("Burn DIAM", async () => {
+    const Wallet = Keypair.generate();
+    const AirdropSignature = await connection.requestAirdrop(
+      Wallet.publicKey,
+      LAMPORTS_PER_SOL
+    );
+
+    await connection.confirmTransaction(AirdropSignature);
+
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const TokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      await randomPayer(),
+      diamMint,
+      Wallet.publicKey
+    );
+
+    const usdcTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      await randomPayer(),
+      usdcMint,
+      Wallet.publicKey
+    );
+
+    await mintTo(
+      connection, // connection to Solana
+      await randomPayer(), // randomPayer as payer for test
+      usdcMint, // USDC Token Mint
+      usdcTokenAccount.address, // User USDC Token Account (destination)
+      tokenAuthority, // Mint Authority (required as signer)
+      initialAmount
+    );
+
+    try {
+      await program.rpc.mintTo(new anchor.BN(initialAmount), {
+        accounts: {
+          merchant: diam.publicKey,
+          mintPda: diamMint,
+          userToken: TokenAccount.address,
+          userUsdcToken: usdcTokenAccount.address,
+          user: Wallet.publicKey,
+          programUsdcToken: usdcPDA,
+          tokenProgram: TOKEN_PROGRAM_ID,
+        },
+        signers: [Wallet],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const balance = (
+      await connection.getTokenAccountBalance(TokenAccount.address)
+    ).value.amount;
+    console.log("Before Burn DIAM Token Balance:", balance);
+
+    const Merchant = Keypair.generate();
+
+    const merchantUsdcTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      await randomPayer(),
+      usdcMint,
+      Merchant.publicKey
+    );
+
+    try {
+      await program.rpc.burn(new anchor.BN(initialAmount / 2), {
+        accounts: {
+          mintPda: diamMint,
+          userToken: TokenAccount.address,
+          user: Wallet.publicKey,
+
+          programUsdcToken: usdcPDA,
+          usdcMint: usdcMintAddress,
+          merchantUsdcToken: merchantUsdcTokenAccount.address,
+
+          tokenProgram: TOKEN_PROGRAM_ID,
+        },
+        signers: [Wallet],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const balance2 = (
+      await connection.getTokenAccountBalance(TokenAccount.address)
+    ).value.amount;
+
+    const balance5 = (
+      await connection.getTokenAccountBalance(merchantUsdcTokenAccount.address)
+    ).value.amount;
+
+    const balance6 = (await connection.getTokenAccountBalance(usdcPDA)).value
+      .amount;
+
+    // assert.equal(balance2, initialAmount / 2);
+
+    console.log("After Burn DIAM Token Balance:", balance2);
+    console.log("Merchant USDC Token Balance:", balance5);
+    console.log("Program USDC Token Balance:", balance6);
   });
 });
+
+// @ts-ignore
+// solana-keygen new --outfile .keys/usdc_mint.json
+const usdcData = JSON.parse(fs.readFileSync(".keys/usdc_mint.json"));
+const usdcMintKeypair = Keypair.fromSecretKey(new Uint8Array(usdcData));
+const usdcMintAddress = usdcMintKeypair.publicKey;
+console.log("USDC Mint:", usdcMintAddress.toString());
+
+// @ts-ignore
+// solana-keygen new --outfile .keys/diam_mint.json
+const diamData = JSON.parse(fs.readFileSync(".keys/diam_mint.json"));
+const diamKeypair = Keypair.fromSecretKey(new Uint8Array(diamData));
+const diamAddress = diamKeypair.publicKey;
+console.log("DIAM Merchant:", diamAddress.toString());
